8a7c25afd332c991f101a97be7f160e20ea2be31
toonjxx <thanhtrungjt@gmail.com>
2023-11-24 13:36:43 +0000

    init

---
+++ model/convnext.py
@@ -0,0 +1,108 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from timm.models.layers import trunc_normal_, DropPath
+from models.utils import LayerNorm
+
+class Block(nn.Module):
+    r""" ConvNeXt Block. There are two equivalent implementations:
+    (1) DwConv -> LayerNorm (channels_first) -> 1x1 Conv -> GELU -> 1x1 Conv; all in (N, C, H, W)
+    (2) DwConv -> Permute to (N, H, W, C); LayerNorm (channels_last) -> Linear -> GELU -> Linear; Permute back
+    
+    Args:
+        dim (int): Number of input channels.
+        drop_path (float): Stochastic depth rate. Default: 0.0
+        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
+    """
+    def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6,dim_mul=4,dwconv_kernel_size=7,dwconv_padding=3):
+        super().__init__()
+        self.dwconv = nn.Conv1d(dim, dim, kernel_size=dwconv_kernel_size, padding=dwconv_padding, groups=dim) # depthwise conv
+        self.norm = LayerNorm(dim, eps=1e-6,data_format="channels_first")
+        self.pwconv1 = nn.Conv1d(dim, dim_mul * dim, kernel_size=1, stride=1, padding=0, groups=1) # pointwise/1x1 convs, implemented with linear layers
+        self.act = nn.GELU()
+        self.pwconv2 = nn.Conv1d(dim_mul*dim, dim, kernel_size=1, stride=1, padding=0, groups=1)
+        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)), 
+                                    requires_grad=True) if layer_scale_init_value > 0 else None
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, x):
+        input = x
+        x = self.dwconv(x)
+        x = self.norm(x)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        x = x.permute(0, 2, 1)
+        if self.gamma is not None:
+            x = self.gamma * x
+        x = x.permute(0, 2, 1)
+        x = input + self.drop_path(x)
+        return x
+
+class ConvNeXt(nn.Module):
+    r""" 
+    Args:
+        in_chans (int): Number of input image channels. Default: 3
+        num_classes (int): Number of classes for classification head. Default: 1000
+        depths (tuple(int)): Number of blocks at each stage. Default: [3, 3, 9, 3]
+        dims (int): Feature dimension at each stage. Default: [96, 192, 384, 768]
+        drop_path_rate (float): Stochastic depth rate. Default: 0.
+        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
+        head_init_scale (float): Init scaling value for classifier weights and biases. Default: 1.
+    """
+    def __init__(self, in_chans=1, num_classes=1,
+                 depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], drop_path_rate=0., 
+                 layer_scale_init_value=1e-6, head_init_scale=1.,dim_mul=2,dwconv_kernel_size=3,dwconv_padding=1,downsample_stem=2
+                 ):
+        super().__init__()
+
+        self.downsample_layers = nn.ModuleList() # stem and 3 intermediate downsampling conv layers
+        stem = nn.Sequential(
+            nn.Conv1d(in_chans, dims[0], kernel_size=downsample_stem, stride=downsample_stem),
+            LayerNorm(dims[0], eps=1e-6)
+        )
+        self.downsample_layers.append(stem)
+        for i in range(3):
+            downsample_layer = nn.Sequential(
+                    LayerNorm(dims[i], eps=1e-6),
+                    nn.Conv1d(dims[i], dims[i+1], kernel_size=2, stride=2),
+            )
+            self.downsample_layers.append(downsample_layer)
+
+        self.stages = nn.ModuleList() # 4 feature resolution stages, each consisting of multiple residual blocks
+        dp_rates=[x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))] 
+        cur = 0
+        for i in range(4):
+            stage = nn.Sequential(
+                *[Block(dim=dims[i], drop_path=dp_rates[cur + j], 
+                layer_scale_init_value=layer_scale_init_value,
+                dim_mul=dim_mul,dwconv_kernel_size=dwconv_kernel_size,dwconv_padding=dwconv_padding) for j in range(depths[i])]
+            )
+            self.stages.append(stage)
+            cur += depths[i]
+
+        self.norm = LayerNorm(dims[-1], eps=1e-6) # final norm layer
+        self.head = nn.Linear(dims[-1], num_classes)
+
+        self.apply(self._init_weights)
+        self.head.weight.data.mul_(head_init_scale)
+        self.head.bias.data.mul_(head_init_scale)
+
+    def _init_weights(self, m):
+        if isinstance(m, (nn.Conv1d, nn.Linear)):
+            trunc_normal_(m.weight, std=.02)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+    def forward_features(self, x):
+        for i in range(4):
+            x = self.downsample_layers[i](x)
+            x = self.stages[i](x)   
+        x = x.mean([-1]) # global average pooling
+        return self.norm(x.unsqueeze(-1)).squeeze(-1)
+
+    def forward(self, x):
+        x = x.permute(0,2,1)
+        x = self.forward_features(x)
+        x = self.head(x)
+        return x
---
+++ src/Inference.py
@@ -0,0 +1,40 @@
+import torch 
+import numpy as np 
+from config import CONFIG
+
+from typing import Dict, Any
+
+class Inference:
+    def __init__(self, package: dict) -> None:
+        self.model = torch.load("model/base.pt")
+        self.classmap = {
+            0: 'low risk of hypertension',
+            1: 'high risk of hypertension'
+        }
+    
+    def _preprocess_request(self, request : Dict[str, Any]) -> Dict[str, Any]:
+        """Preprocess request
+
+        Args:
+            package (dict): The package used by FastAPI to get the scaler 
+            request (Dict[str, Any]): The request we will be getting 
+
+        Returns:
+            Dict[str, Any]: Preprocessed request
+        """
+        
+        data = request['ppg']
+
+        return torch.tensor(data, dtype=torch.float32).unsqueeze(0).to('cuda')
+    
+
+    def predict(self, request: dict) -> dict:
+        preprocessed_request = self._preprocess_request(request)
+        model = self.model
+        model.eval()
+        model.to('cuda')
+        predictions = model(preprocessed_request)
+        predicted_class = torch.sigmoid(predictions).round()
+        return {
+            'prediction' : self.classmap[predicted_class]
+        }
